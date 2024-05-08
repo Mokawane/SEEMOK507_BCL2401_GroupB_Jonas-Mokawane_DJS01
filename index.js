68564f93033e796b7cd20eb8c6db632bf716f8e0
Jonas Mokawane <jonas.mokawane98@gmail.com>
2024-05-08 15:30:41 +0200

    Converted time from seconds to hours and velocity m/s to km/h

--- index.js
+++ index.js
@@ -14,19 +14,13 @@ const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
 const calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+  return vel + (acc*time)*(time/1000)
 }
 
-const d2 = d + (vel*time) //calcultes new distance
+const d2 = d + (vel*time/time)//calcultes new distance
 const rf = fuel - fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const vel2 = calcNewVel(vel, acc, time) //calculates new velocity based on acceleration
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
 console.log(`Corrected New Distance: ${d2} km`);
 console.log(`Corrected Remaining Fuel: ${rf} kg`);
-
-
-
-
-
-
